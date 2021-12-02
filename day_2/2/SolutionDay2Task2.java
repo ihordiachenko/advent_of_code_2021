c3d63a761ac17d383f2f0681bd9751f642f8b823
Ihor Diachenko <ih.diachenko@gmail.com>
2021-12-03 00:29:17 +0200

    Added solutions for day 2

---
+++ day_2/2/SolutionDay2Task2.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class SolutionDay2Task2 {
+    public static void main(String[] args) {
+        try {
+            var fs = new FileInputStream("./day_2/2/input.txt");
+            var br = new BufferedReader(new InputStreamReader(fs));
+
+            int depth = 0, horizontalPosition = 0, aim = 0;
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] lineArgs = line.split(" ");
+                String directrion = lineArgs[0];
+                int delta = Integer.parseInt(lineArgs[1]);
+
+                if (directrion.equals("up")) {
+                    aim -= delta;
+                } else if (directrion.equals("down")) {
+                    aim += delta;
+                } else {
+                    horizontalPosition += delta;
+                    depth += aim*delta;
+                }
+            }
+
+            System.out.println(depth*horizontalPosition);
+            br.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
