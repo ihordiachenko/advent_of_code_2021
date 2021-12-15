69e8066b2fa987f7e138213f1166de98688a01e6
Ihor Diachenko <ih.diachenko@gmail.com>
2021-12-16 00:29:19 +0200

    Added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2021 Solutions in Java
+
+It ain't much, but it's honest work.
--- day_14/Day14Task2.java
+++ day_14/Day14Task2.java
@@ -42,10 +42,6 @@ public class Day14Task2 {
         }
     }
 
-
-
-
-
     static Map<String, Long> dp(
         String duplet,
         int daysLeft,
